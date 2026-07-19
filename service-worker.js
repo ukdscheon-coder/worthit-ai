b968c1b8963f1d03d66e9e639dd56153702155e5
Daeseong Cheon <uk.dscheon@gmail.com>
2026-07-19 23:43:44 +0100

    Refresh cache for charge-focused app

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,19 @@
-const CACHE='worthit-hub-v2';
-const ASSETS=['/','/index.html','/style.css?v=worthit-hub-1','/app.js?v=worthit-hub-2','/manifest.json','/icon.svg'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('/index.html'))))});
+const CACHE='worthit-charge-focused-v1';
+const ASSETS=['/','/index.html','/style.css?v=charge-focused-1','/app.js?v=charge-focused-1','/manifest.json','/icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('/index.html'))))
+});
